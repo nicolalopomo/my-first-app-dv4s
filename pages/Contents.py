59df46ae6b47b9d6ee1ab9cc1ef2a0d7ed911a06
nicolalopomo <nicola.lopomo@polimi.it>
2026-03-22 21:26:18 +0000

    Other modifications

---
+++ pages/Contents.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Contents Page")
+
+st.write("This is the contents page.")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="My App",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
 st.title("My New STREAMLIT app")
 
 st.write("Welcome to my new app!!!")
@@ -7,4 +13,4 @@ st.write("Welcome to my new app!!!")
 
 data = st.slider("Select a value", 0, 100, 50)
 
-st.write("You selected:", data)
+st.write("You selected :", data)
